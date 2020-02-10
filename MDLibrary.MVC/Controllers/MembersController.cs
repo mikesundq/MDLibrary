6b590309e745874c794ed31d0e711f864d4cff54
mikesundq <55479605+mikesundq@users.noreply.github.com>
2020-02-10 13:32:15 +0100

    added edit member

--- MDLibrary.MVC/Controllers/MembersController.cs
+++ MDLibrary.MVC/Controllers/MembersController.cs
@@ -68,66 +68,53 @@ namespace MDLibrary.MVC.Controllers
             if (!ModelState.IsValid)
                 return View(vm);
 
-            var newMembertoAdd = new Member();
-            newMembertoAdd.Name = vm.Name;
-            newMembertoAdd.SSN = vm.SSN;
-            memberService.AddNewMember(newMembertoAdd);
+            var newMemberToAdd = new Member();
+            newMemberToAdd.Name = vm.Name;
+            newMemberToAdd.SSN = vm.SSN;
+            memberService.AddNewMember(newMemberToAdd);
             
             return RedirectToAction(nameof(Index));
             
         }
-        /*
+        
         // GET: Members/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> Edit(int id)
         {
-            if (id == null)
+            var memberToEdit = memberService.GetMemberById(id);
+            if (memberToEdit == null)
             {
                 return NotFound();
             }
+            var editMemberVm = new EditMemberVm();
+            editMemberVm.Name = memberToEdit.Name;
+            editMemberVm.SSN = memberToEdit.SSN;
 
-            var member = await _context.Member.FindAsync(id);
-            if (member == null)
-            {
-                return NotFound();
-            }
-            return View(member);
+            return View(editMemberVm);
         }
-
+        
         // POST: Members/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,SSN,Name")] Member member)
+        public async Task<IActionResult> Edit(int id, [Bind("ID, SSN,Name")] Member member)
         {
-            if (id != member.ID)
-            {
-                return NotFound();
-            }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(member);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MemberExists(member.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(member);
+
+            if (id != member.ID)
+                return NotFound();
+
+            if (!ModelState.IsValid)
+                return View(member);
+
+
+
+            memberService.EditMember(member);
+
+
+            return RedirectToAction(nameof(Index));
         }
-        */
+        
         // GET: Members/Delete/5
         public async Task<IActionResult> Delete(int id)
         {
---
+++ MDLibrary.MVC/Models/MemberVM/EditMemberVm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MDLibrary.MVC.Models.MemberVM
+{
+    public class EditMemberVm
+    {
+        //[ReadOnly(true)]
+        //public int Id { get; private set; }
+        [Required]
+        [StringLength(10, MinimumLength = 10)]
+        [Display(Name = "Social Security Number")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "This is not a valid SSN")]
+        public string SSN { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
