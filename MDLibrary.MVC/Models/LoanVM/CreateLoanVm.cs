e2c8031302e8df97798e5a3f0329dd820b1f7506
MrDanielNy <tjanster1@danielny.se>
2020-02-25 09:23:12 +0100

    add late lone to db and fix back

--- MDLibrary.MVC/Models/LoanVM/CreateLoanVm.cs
+++ MDLibrary.MVC/Models/LoanVM/CreateLoanVm.cs
@@ -17,7 +17,7 @@ namespace MDLibrary.MVC.Models.LoanVM
         [Display(Name = "Time of loan")]
         public string DisplayDate { get; set; } = DateTime.Today.ToString("yyyy-MM-dd");
         [DisplayName("Date to return loan")]
-        public string TimeToReturnBook { get; set; } = DateTime.Today.AddDays(14).ToString("yyyy-MM-dd");
+        public string TimeToReturnBook { get; set; } = DateTime.Today.ToString("yyyy-MM-dd");
         [DisplayName("Books")]
         public IList<BookDetails> Books { get; set; }
         public IList<BookCopy> AvalibleBooks { get; set; }
